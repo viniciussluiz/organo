5a02a0d99d6711270367636a8cb15ea54aba4fc3
Vinicius <vinicius.luiz@sga.pucminas.br>
2023-04-27 14:50:16 -0300

    Primeiro commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+        <img src="/imagens/banner.png" alt="Imagem do cabeçalho"/>
+        </header>
+    )
+}
+
+export default Banner
