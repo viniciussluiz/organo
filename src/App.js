67a67c2d61e63732e98f0f88257c35516e6c0723
Vinicius <vinicius.luiz@sga.pucminas.br>
2023-04-28 11:22:09 -0300

    Adicionando campos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Banner from "./componentes/Banner/Banner";
+import Banner from "./componentes/Banner";
+import CampoTexto from "./componentes/CampoTexto";
 
 function App() {
   return (
     <div className="App">
       <Banner />
+        <CampoTexto label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
     </div>
   );
 }
